8445eaee329a7288e5cdffcd3af12e8cc4efab0d
Ernest <zyperis14@gmail.com>
2025-05-02 11:49:58 +0300

    opositeSting

---
+++ opositeString.js
@@ -0,0 +1,44 @@
+function isOpposite(s1, s2) {
+    if (s1.length === 0 || s2.length === 0) {
+        return false
+    }
+
+    if (s1.length !== s2.length) {
+        return false
+    }
+
+    let count = 0;
+
+    for (let i = 0; i < s1.length; i++) {
+        if (s1[i].charCodeAt() <= 90 && s2[i].charCodeAt() >= 97) {
+            count++
+        }
+        if (s1[i].charCodeAt() >= 97 && s2[i].charCodeAt() <= 90) {
+            count++
+        }
+    }
+
+    if (count === s1.length) {
+        return true;
+    } else {
+        return false;
+    }
+}
+//
+function isOpposite2(s1, s2) {
+    if (s1 === s2 || s1.toLowerCase() !== s2.toLowerCase()) return false;
+
+    for (var i = 0; i < s1.length; i++) {
+        if (s1.charAt(i) === s2.charAt(i)) return false;
+    }
+
+    return true;
+
+}
+
+console.log(isOpposite("ab", "AB"), true);
+console.log(isOpposite("aB", "Ab"), true);
+console.log(isOpposite("aBcd", "AbCD"), true);
+console.log(isOpposite("aBcde", "AbCD"), false);
+console.log(isOpposite("", ""), false);
+console.log(isOpposite("AB", "Ab"), false);
